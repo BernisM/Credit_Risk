051d63687183827eb4d247beb98bcd9460c50849
BernisM <hellobernis@outlook.com>
2023-07-09 23:12:38 +0200

    sunday night

--- PD_model.py
+++ PD_model.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import re
+import os
 
+os.chdir("C:/Users/massw/OneDrive/Bureau/Programmation/Python_R/Credit_Risk")
 loan_data_backup = pd.read_csv('loan_data_2007_2014.csv')
 
 loan_data = loan_data_backup.copy()
@@ -41,6 +43,7 @@ loan_data['term']
 loan_data['term_months'] = pd.to_numeric(loan_data['term'].str.replace(' months', ''))
 
 loan_data['term_days'] = loan_data['term_months'] * 30
+
 
 # Preprocessing for issue Date
 loan_data['issue_d']
@@ -211,6 +214,286 @@ df1['IV'] = df1['IV'].sum()
 
 df1
 
+# Preprocessing discrete variables : Automation Calculations
+
+# WoE function for discrete unordered variables
+# Here we combine all of the operations above in a function.
+# The function takes 3 arguments: a dataframe, a string, and a dataframe. The function returns a dataframe as a result.
+def woe_discrete(df, discrete_variable_name, good_bad_variable_df):
+    df = pd.concat([df[discrete_variable_name], good_bad_variable_df], axis=1)
+    df = pd.concat([df.groupby(df.columns.values[0], as_index = False)[df.columns.values[1]].count(),
+                df.groupby(df.columns.values[0], as_index = False)[df.columns.values[1]].mean()], axis = 1)
+    df = df.iloc[:, [0, 1, 3]]
+    df.columns = [df.columns.values[0], 'n_obs', 'prop_good']
+    df['prop_n_obs'] = df['n_obs'] / df['n_obs'].sum()
+    df['n_good'] = df['prop_good'] * df['n_obs']
+    df['n_bad'] = (1 - df['prop_good']) * df['n_obs']
+    df['prop_n_good'] = df['n_good'] / df['n_good'].sum()
+    df['prop_n_bad'] = df['n_bad'] / df['n_bad'].sum()
+    df['WoE'] = np.log(df['prop_n_good'] / df['prop_n_bad'])
+    df = df.sort_values(['WoE'])
+    df = df.reset_index(drop = True)
+    df['diff_prop_good'] = df['prop_good'].diff().abs()
+    df['diff_WoE'] = df['WoE'].diff().abs()
+    df['IV'] = (df['prop_n_good'] - df['prop_n_bad']) * df['WoE']
+    df['IV'] = df['IV'].sum()
+    return df
+
+# We execute the function we defined with the necessary arguments: a dataframe, a string, and a dataframe.
+# We store the result in a dataframe.
+df_temp = woe_discrete(df_inputs_prep, 'grade', df_targets_prepr)
+df_temp
+
+# Preprocessing discrete variables : visualizing result
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+def plot_by_woe(df_WoE, rotation_of_x_axis_labels = 0):
+    x = np.array(df_WoE.iloc[:, 0].apply(str))
+    # Turns the values of the column with index 0 to strings, makes an array from these strings, and passes it to variable x.
+    y = df_WoE['WoE']
+    # Selects a column with label 'WoE' and passes it to variable y.
+    plt.figure(figsize=(18, 6))
+    # Sets the graph size to width 18 x height 6.
+    plt.plot(x, y, marker = 'o', linestyle = '--', color = 'k')
+    # Plots the datapoints with coordiantes variable x on the x-axis and variable y on the y-axis.
+    # Sets the marker for each datapoint to a circle, the style line between the points to dashed, and the color to black.
+    plt.xlabel(df_WoE.columns[0])
+    # Names the x-axis with the name of the column with index 0.
+    plt.ylabel('Weight of Evidence')
+    # Names the y-axis 'Weight of Evidence'.
+    plt.title(str('Weight of Evidence by ' + df_WoE.columns[0]))
+    # Names the grapth 'Weight of Evidence by ' the name of the column with index 0.
+    plt.xticks(rotation = rotation_of_x_axis_labels)
+    # Rotates the labels of the x-axis a predefined number of degrees.
+    plt.show()
+
+# We execute the function we defined with the necessary arguments: a dataframe.
+# We omit the number argument, which means the function will use its default value, 0.
+plot_by_woe(df_temp)
+
+# Preprocessing discrete variables : Creating Dummy Variable, Part 1 
+
+# 'home_ownership'
+df_temp = woe_discrete(df_inputs_prep, 'home_ownership', df_targets_prepr)
 
 
+# We plot the weight of evidence values.
+plot_by_woe(df_temp)
 
+# There are many categories with very few observations and many categories with very different "good" %.
+# Therefore, we create a new discrete variable where we combine some of the categories.
+# 'OTHERS' and 'NONE' are riskiest but are very few. 'RENT' is the next riskiest.
+# 'ANY' are least risky but are too few. Conceptually, they belong to the same category. Also, their inclusion would not change anything.
+# We combine them in one category, 'RENT_OTHER_NONE_ANY'.
+# We end up with 3 categories: 'RENT_OTHER_NONE_ANY', 'OWN', 'MORTGAGE'.
+df_inputs_prep['home_ownership:RENT_OTHER_NONE_ANY'] = sum([df_inputs_prep['home_ownership:RENT'], df_inputs_prep['home_ownership:OTHER'],
+                                                      df_inputs_prep['home_ownership:NONE'],df_inputs_prep['home_ownership:ANY']])
+# 'RENT_OTHER_NONE_ANY' will be the reference category.
+
+# Alternatively:
+#loan_data.loc['home_ownership' in ['RENT', 'OTHER', 'NONE', 'ANY'], 'home_ownership:RENT_OTHER_NONE_ANY'] = 1
+#loan_data.loc['home_ownership' not in ['RENT', 'OTHER', 'NONE', 'ANY'], 'home_ownership:RENT_OTHER_NONE_ANY'] = 0
+#loan_data.loc['loan_status' not in ['OWN'], 'home_ownership:OWN'] = 1
+#loan_data.loc['loan_status' not in ['OWN'], 'home_ownership:OWN'] = 0
+#loan_data.loc['loan_status' not in ['MORTGAGE'], 'home_ownership:MORTGAGE'] = 1
+#loan_data.loc['loan_status' not in ['MORTGAGE'], 'home_ownership:MORTGAGE'] = 0
+
+
+# Preprocessing discrete variables : Creating Dummy Variable, Part 2
+df_temp = woe_discrete(df_inputs_prep, 'addr_state', df_targets_prepr)
+df_temp
+
+plot_by_woe(df_temp)
+
+if ['addr_state:ND'] in df_inputs_prep.columns.values:
+    pass
+else:
+    df_inputs_prep['addr_state:ND'] = 0
+
+plot_by_woe(df_temp.iloc[2: -2, : ])
+# We plot the weight of evidence values.
+
+plot_by_woe(df_temp.iloc[6: -6, : ])
+# We plot the weight of evidence values.
+
+# We create the following categories:
+# 'ND' 'NE' 'IA' NV' 'FL' 'HI' 'AL'
+# 'NM' 'VA'
+# 'NY'
+# 'OK' 'TN' 'MO' 'LA' 'MD' 'NC'
+# 'CA'
+# 'UT' 'KY' 'AZ' 'NJ'
+# 'AR' 'MI' 'PA' 'OH' 'MN'
+# 'RI' 'MA' 'DE' 'SD' 'IN'
+# 'GA' 'WA' 'OR'
+# 'WI' 'MT'
+# 'TX'
+# 'IL' 'CT'
+# 'KS' 'SC' 'CO' 'VT' 'AK' 'MS'
+# 'WV' 'NH' 'WY' 'DC' 'ME' 'ID'
+
+# 'IA_NV_HI_ID_AL_FL' will be the reference category.
+
+df_inputs_prep['addr_state:ND_NE_IA_NV_FL_HI_AL'] = sum([df_inputs_prep['addr_state:ND'], df_inputs_prep['addr_state:NE'],
+                                              df_inputs_prep['addr_state:IA'], df_inputs_prep['addr_state:NV'],
+                                              df_inputs_prep['addr_state:FL'], df_inputs_prep['addr_state:HI'],
+                                                          df_inputs_prep['addr_state:AL']])
+
+df_inputs_prep['addr_state:NM_VA'] = sum([df_inputs_prep['addr_state:NM'], df_inputs_prep['addr_state:VA']])
+
+df_inputs_prep['addr_state:OK_TN_MO_LA_MD_NC'] = sum([df_inputs_prep['addr_state:OK'], df_inputs_prep['addr_state:TN'],
+                                              df_inputs_prep['addr_state:MO'], df_inputs_prep['addr_state:LA'],
+                                              df_inputs_prep['addr_state:MD'], df_inputs_prep['addr_state:NC']])
+
+df_inputs_prep['addr_state:UT_KY_AZ_NJ'] = sum([df_inputs_prep['addr_state:UT'], df_inputs_prep['addr_state:KY'],
+                                              df_inputs_prep['addr_state:AZ'], df_inputs_prep['addr_state:NJ']])
+
+df_inputs_prep['addr_state:AR_MI_PA_OH_MN'] = sum([df_inputs_prep['addr_state:AR'], df_inputs_prep['addr_state:MI'],
+                                              df_inputs_prep['addr_state:PA'], df_inputs_prep['addr_state:OH'],
+                                              df_inputs_prep['addr_state:MN']])
+
+df_inputs_prep['addr_state:RI_MA_DE_SD_IN'] = sum([df_inputs_prep['addr_state:RI'], df_inputs_prep['addr_state:MA'],
+                                              df_inputs_prep['addr_state:DE'], df_inputs_prep['addr_state:SD'],
+                                              df_inputs_prep['addr_state:IN']])
+
+df_inputs_prep['addr_state:GA_WA_OR'] = sum([df_inputs_prep['addr_state:GA'], df_inputs_prep['addr_state:WA'],
+                                              df_inputs_prep['addr_state:OR']])
+
+df_inputs_prep['addr_state:WI_MT'] = sum([df_inputs_prep['addr_state:WI'], df_inputs_prep['addr_state:MT']])
+
+df_inputs_prep['addr_state:IL_CT'] = sum([df_inputs_prep['addr_state:IL'], df_inputs_prep['addr_state:CT']])
+
+df_inputs_prep['addr_state:KS_SC_CO_VT_AK_MS'] = sum([df_inputs_prep['addr_state:KS'], df_inputs_prep['addr_state:SC'],
+                                              df_inputs_prep['addr_state:CO'], df_inputs_prep['addr_state:VT'],
+                                              df_inputs_prep['addr_state:AK'], df_inputs_prep['addr_state:MS']])
+
+df_inputs_prep['addr_state:WV_NH_WY_DC_ME_ID'] = sum([df_inputs_prep['addr_state:WV'], df_inputs_prep['addr_state:NH'],
+                                              df_inputs_prep['addr_state:WY'], df_inputs_prep['addr_state:DC'],
+                                              df_inputs_prep['addr_state:ME'], df_inputs_prep['addr_state:ID']])
+
+
+# Preprocessing discrete variables : Creating Dummy Variable, Part 3 : VERIFICATION STATUS
+df_temp = woe_discrete(df_inputs_prep, 'verification_status', df_targets_prepr)
+plot_by_woe(df_temp)
+
+# Preprocessing discrete variables : Creating Dummy Variable, Part 3 : PURPOSE
+df_temp = woe_discrete(df_inputs_prep, 'purpose', df_targets_prepr)
+plot_by_woe(df_temp,90)
+
+df_inputs_prep['purpose:educ__sm_b__wedd__ren_en__mov__house'] = sum([df_inputs_prep['purpose:small_business'], 
+                                                                 df_inputs_prep['purpose:educational'],
+                                                                 df_inputs_prep['purpose:moving'],
+                                                                 df_inputs_prep['purpose:house'],
+                                                                 df_inputs_prep['purpose:renewable_energy'],
+                                                                 df_inputs_prep['purpose:wedding']])
+
+df_inputs_prep['purpose:oth__med__vacation'] = sum([df_inputs_prep['purpose:other'],
+                                                    df_inputs_prep['purpose:medical'],
+                                                    df_inputs_prep['purpose:vacation']])
+
+df_inputs_prep['purpose:major_purch__car__home_impr'] = sum([df_inputs_prep['purpose:major_purchase'], df_inputs_prep['purpose:car'],
+                                                        df_inputs_prep['purpose:home_improvement']])
+
+
+# Preprocessing discrete variables : Creating Dummy Variable, Part 3 : INITIAL LIST STATUS
+df_temp = woe_discrete(df_inputs_prep, 'initial_list_status', df_targets_prepr)
+plot_by_woe(df_temp)
+
+
+# Preprocessing Continuous Variables: Automating Calculations and Visualizing Results
+# WoE function for ordered discrete and continuous variables
+def woe_ordered_continuous(df, discrete_variabe_name, good_bad_variable_df):
+    df = pd.concat([df[discrete_variabe_name], good_bad_variable_df], axis = 1)
+    df = pd.concat([df.groupby(df.columns.values[0], as_index = False)[df.columns.values[1]].count(),
+                    df.groupby(df.columns.values[0], as_index = False)[df.columns.values[1]].mean()], axis = 1)
+    df = df.iloc[:, [0, 1, 3]]
+    df.columns = [df.columns.values[0], 'n_obs', 'prop_good']
+    df['prop_n_obs'] = df['n_obs'] / df['n_obs'].sum()
+    df['n_good'] = df['prop_good'] * df['n_obs']
+    df['n_bad'] = (1 - df['prop_good']) * df['n_obs']
+    df['prop_n_good'] = df['n_good'] / df['n_good'].sum()
+    df['prop_n_bad'] = df['n_bad'] / df['n_bad'].sum()
+    df['WoE'] = np.log(df['prop_n_good'] / df['prop_n_bad'])
+    #df = df.sort_values(['WoE'])
+    #df = df.reset_index(drop = True)
+    df['diff_prop_good'] = df['prop_good'].diff().abs()
+    df['diff_WoE'] = df['WoE'].diff().abs()
+    df['IV'] = (df['prop_n_good'] - df['prop_n_bad']) * df['WoE']
+    df['IV'] = df['IV'].sum()
+    return df
+# Here we define a function similar to the one above, ...
+# ... with one slight difference: we order the results by the values of a different column.
+# The function takes 3 arguments: a dataframe, a string, and a dataframe. The function returns a dataframe as a result.
+
+df_inputs_prep['term_months'].unique()
+
+df_temp = woe_ordered_continuous(df_inputs_prep, 'term_months', df_targets_prepr)
+df_temp
+
+plot_by_woe(df_temp)
+
+df_inputs_prep['term:36'] = np.where((df_inputs_prep['term_months'] == 36), 1, 0)
+df_inputs_prep['term:60'] = np.where((df_inputs_prep['term_months'] == 60), 1, 0)
+
+df_inputs_prep['emp_length_int'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'emp_length_int', df_targets_prepr)
+plot_by_woe(df_temp)
+
+df_inputs_prep['emp_length:0'] = np.where(df_inputs_prep['emp_length_int'].isin([0]), 1, 0)
+df_inputs_prep['emp_length:1'] = np.where(df_inputs_prep['emp_length_int'].isin([1]), 1, 0)
+df_inputs_prep['emp_length:2-4'] = np.where(df_inputs_prep['emp_length_int'].isin(range(2, 5)), 1, 0)
+df_inputs_prep['emp_length:5-6'] = np.where(df_inputs_prep['emp_length_int'].isin(range(5, 7)), 1, 0)
+df_inputs_prep['emp_length:7-9'] = np.where(df_inputs_prep['emp_length_int'].isin(range(7, 10)), 1, 0)
+df_inputs_prep['emp_length:10'] = np.where(df_inputs_prep['emp_length_int'].isin([10]), 1, 0)
+
+# Preprocessing Continuous Variables: Automating Calculations and Visualizing Results, Part 2
+df_inputs_prep['months_issue_date'].unique()
+
+df_inputs_prep['months_issue_date_factors'] = pd.cut(df_inputs_prep['months_issue_date'], 50)
+df_temp = woe_ordered_continuous(df_inputs_prep, 'months_issue_date_factors', df_targets_prepr)
+df_temp
+plot_by_woe(df_temp, 90)
+
+df_inputs_prep['months_issue_date:<38'] = np.where(df_inputs_prep['months_issue_date'].isin(range(38)), 1, 0)
+df_inputs_prep['months_issue_date:38-39'] = np.where(df_inputs_prep['months_issue_date'].isin(range(38, 40)), 1, 0)
+df_inputs_prep['months_issue_date:40-41'] = np.where(df_inputs_prep['months_issue_date'].isin(range(40, 42)), 1, 0)
+df_inputs_prep['months_issue_date:42-48'] = np.where(df_inputs_prep['months_issue_date'].isin(range(42, 49)), 1, 0)
+df_inputs_prep['months_issue_date:49-52'] = np.where(df_inputs_prep['months_issue_date'].isin(range(49, 53)), 1, 0)
+df_inputs_prep['months_issue_date:53-64'] = np.where(df_inputs_prep['months_issue_date'].isin(range(53, 65)), 1, 0)
+df_inputs_prep['months_issue_date:65-84'] = np.where(df_inputs_prep['months_issue_date'].isin(range(65, 85)), 1, 0)
+df_inputs_prep['months_issue_date:>84'] = np.where(df_inputs_prep['months_issue_date'].isin(range(85, int(df_inputs_prep['months_issue_date'].max()))), 1, 0)
+
+
+df_inputs_prep['int_rate_factor'] = pd.cut(df_inputs_prep['int_rate'],50)
+df_temp = woe_ordered_continuous(df_inputs_prep, 'int_rate_factor', df_targets_prepr)
+plot_by_woe(df_temp, 90)
+
+df_inputs_prep['int_rate:<9.548'] = np.where((df_inputs_prep['int_rate'] <= 9.548), 1, 0)
+df_inputs_prep['int_rate:9.548-12.025'] = np.where((df_inputs_prep['int_rate'] > 9.548) & (df_inputs_prep['int_rate'] <= 12.025), 1, 0)
+df_inputs_prep['int_rate:12.025-15.74'] = np.where((df_inputs_prep['int_rate'] > 12.025) & (df_inputs_prep['int_rate'] <= 15.74), 1, 0)
+df_inputs_prep['int_rate:15.74-20.281'] = np.where((df_inputs_prep['int_rate'] > 15.74) & (df_inputs_prep['int_rate'] <= 20.281), 1, 0)
+df_inputs_prep['int_rate:>20.281'] = np.where((df_inputs_prep['int_rate'] > 20.281), 1, 0)
+
+
+df_inputs_prep['funded_amnt_factor'] = pd.cut(df_inputs_prep['funded_amnt'],50)
+df_temp = woe_ordered_continuous(df_inputs_prep, 'funded_amnt_factor', df_targets_prepr)
+plot_by_woe(df_temp,90)
+
+df_inputs_prep['months_earliest_cr_line'].unique()
+df_inputs_prep['months_earliest_cr_linefactor'] = pd.cut(df_inputs_prep['months_earliest_cr_line'],50)
+df_temp = woe_ordered_continuous(df_inputs_prep, 'months_earliest_cr_linefactor', df_targets_prepr)
+plot_by_woe(df_temp, 90)
+
+df_inputs_prep['months_earliest_cr_line:<140'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(140)), 1, 0)
+df_inputs_prep['months_earliest_cr_line:140-164'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(140, 165)), 1, 0)
+df_inputs_prep['months_earliest_cr_line:165-247'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(165, 248)), 1, 0)
+df_inputs_prep['months_earliest_cr_line:248-270'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(248, 271)), 1, 0)
+df_inputs_prep['months_earliest_cr_line:271-352'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(271, 353)), 1, 0)
+df_inputs_prep['months_earliest_cr_line:>352'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(353, int(df_inputs_prep['months_earliest_cr_line'].max()))), 1, 0)
+
+df_inputs_prep['installment_factor'] = pd.cut(df_inputs_prep['installment'], 50)
+df_temp = woe_ordered_continuous(df_inputs_prep, 'installment_factor', df_targets_prepr)
+plot_by_woe(df_temp)
