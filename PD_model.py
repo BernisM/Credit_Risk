114aa706addc15598f43dd0d3721da4eb8ee0db6
BernisM <hellobernis@outlook.com>
2023-07-11 00:14:39 +0200

    last _ parameters

--- PD_model.py
+++ PD_model.py
@@ -158,6 +158,9 @@ loan_data_inputs_train, loan_data_inputs_test, loan_data_targets_train, loan_dat
 
 df_inputs_prep = loan_data_inputs_train
 df_targets_prepr = loan_data_targets_train
+df_inputs_prep = loan_data_inputs_test
+df_targets_prepr = loan_data_targets_test
+
 
 df_inputs_prep['grade'].unique()
 
@@ -272,7 +275,7 @@ def plot_by_woe(df_WoE, rotation_of_x_axis_labels = 0):
 
 # We execute the function we defined with the necessary arguments: a dataframe.
 # We omit the number argument, which means the function will use its default value, 0.
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 # Preprocessing discrete variables : Creating Dummy Variable, Part 1 
 
@@ -281,7 +284,7 @@ df_temp = woe_discrete(df_inputs_prep, 'home_ownership', df_targets_prepr)
 
 
 # We plot the weight of evidence values.
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 # There are many categories with very few observations and many categories with very different "good" %.
 # Therefore, we create a new discrete variable where we combine some of the categories.
@@ -306,17 +309,17 @@ df_inputs_prep['home_ownership:RENT_OTHER_NONE_ANY'] = sum([df_inputs_prep['home
 df_temp = woe_discrete(df_inputs_prep, 'addr_state', df_targets_prepr)
 df_temp
 
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 if ['addr_state:ND'] in df_inputs_prep.columns.values:
     pass
 else:
     df_inputs_prep['addr_state:ND'] = 0
 
-plot_by_woe(df_temp.iloc[2: -2, : ])
+# plot_by_woe(df_temp.iloc[2: -2, : ])
 # We plot the weight of evidence values.
 
-plot_by_woe(df_temp.iloc[6: -6, : ])
+# plot_by_woe(df_temp.iloc[6: -6, : ])
 # We plot the weight of evidence values.
 
 # We create the following categories:
@@ -377,11 +380,11 @@ df_inputs_prep['addr_state:WV_NH_WY_DC_ME_ID'] = sum([df_inputs_prep['addr_state
 
 # Preprocessing discrete variables : Creating Dummy Variable, Part 3 : VERIFICATION STATUS
 df_temp = woe_discrete(df_inputs_prep, 'verification_status', df_targets_prepr)
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 # Preprocessing discrete variables : Creating Dummy Variable, Part 3 : PURPOSE
 df_temp = woe_discrete(df_inputs_prep, 'purpose', df_targets_prepr)
-plot_by_woe(df_temp,90)
+# plot_by_woe(df_temp,90)
 
 df_inputs_prep['purpose:educ__sm_b__wedd__ren_en__mov__house'] = sum([df_inputs_prep['purpose:small_business'], 
                                                                  df_inputs_prep['purpose:educational'],
@@ -400,7 +403,7 @@ df_inputs_prep['purpose:major_purch__car__home_impr'] = sum([df_inputs_prep['pur
 
 # Preprocessing discrete variables : Creating Dummy Variable, Part 3 : INITIAL LIST STATUS
 df_temp = woe_discrete(df_inputs_prep, 'initial_list_status', df_targets_prepr)
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 
 # Preprocessing Continuous Variables: Automating Calculations and Visualizing Results
@@ -433,14 +436,14 @@ df_inputs_prep['term_months'].unique()
 df_temp = woe_ordered_continuous(df_inputs_prep, 'term_months', df_targets_prepr)
 df_temp
 
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 df_inputs_prep['term:36'] = np.where((df_inputs_prep['term_months'] == 36), 1, 0)
 df_inputs_prep['term:60'] = np.where((df_inputs_prep['term_months'] == 60), 1, 0)
 
 df_inputs_prep['emp_length_int'].unique()
 df_temp = woe_ordered_continuous(df_inputs_prep, 'emp_length_int', df_targets_prepr)
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
 
 df_inputs_prep['emp_length:0'] = np.where(df_inputs_prep['emp_length_int'].isin([0]), 1, 0)
 df_inputs_prep['emp_length:1'] = np.where(df_inputs_prep['emp_length_int'].isin([1]), 1, 0)
@@ -455,7 +458,7 @@ df_inputs_prep['months_issue_date'].unique()
 df_inputs_prep['months_issue_date_factors'] = pd.cut(df_inputs_prep['months_issue_date'], 50)
 df_temp = woe_ordered_continuous(df_inputs_prep, 'months_issue_date_factors', df_targets_prepr)
 df_temp
-plot_by_woe(df_temp, 90)
+# plot_by_woe(df_temp, 90)
 
 df_inputs_prep['months_issue_date:<38'] = np.where(df_inputs_prep['months_issue_date'].isin(range(38)), 1, 0)
 df_inputs_prep['months_issue_date:38-39'] = np.where(df_inputs_prep['months_issue_date'].isin(range(38, 40)), 1, 0)
@@ -469,7 +472,7 @@ df_inputs_prep['months_issue_date:>84'] = np.where(df_inputs_prep['months_issue_
 
 df_inputs_prep['int_rate_factor'] = pd.cut(df_inputs_prep['int_rate'],50)
 df_temp = woe_ordered_continuous(df_inputs_prep, 'int_rate_factor', df_targets_prepr)
-plot_by_woe(df_temp, 90)
+# plot_by_woe(df_temp, 90)
 
 df_inputs_prep['int_rate:<9.548'] = np.where((df_inputs_prep['int_rate'] <= 9.548), 1, 0)
 df_inputs_prep['int_rate:9.548-12.025'] = np.where((df_inputs_prep['int_rate'] > 9.548) & (df_inputs_prep['int_rate'] <= 12.025), 1, 0)
@@ -480,12 +483,12 @@ df_inputs_prep['int_rate:>20.281'] = np.where((df_inputs_prep['int_rate'] > 20.2
 
 df_inputs_prep['funded_amnt_factor'] = pd.cut(df_inputs_prep['funded_amnt'],50)
 df_temp = woe_ordered_continuous(df_inputs_prep, 'funded_amnt_factor', df_targets_prepr)
-plot_by_woe(df_temp,90)
+# plot_by_woe(df_temp,90)
 
 df_inputs_prep['months_earliest_cr_line'].unique()
 df_inputs_prep['months_earliest_cr_linefactor'] = pd.cut(df_inputs_prep['months_earliest_cr_line'],50)
 df_temp = woe_ordered_continuous(df_inputs_prep, 'months_earliest_cr_linefactor', df_targets_prepr)
-plot_by_woe(df_temp, 90)
+# plot_by_woe(df_temp, 90)
 
 df_inputs_prep['months_earliest_cr_line:<140'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(140)), 1, 0)
 df_inputs_prep['months_earliest_cr_line:140-164'] = np.where(df_inputs_prep['months_earliest_cr_line'].isin(range(140, 165)), 1, 0)
@@ -496,4 +499,169 @@ df_inputs_prep['months_earliest_cr_line:>352'] = np.where(df_inputs_prep['months
 
 df_inputs_prep['installment_factor'] = pd.cut(df_inputs_prep['installment'], 50)
 df_temp = woe_ordered_continuous(df_inputs_prep, 'installment_factor', df_targets_prepr)
-plot_by_woe(df_temp)
+# plot_by_woe(df_temp)
+
+df_inputs_prep['delinq_2yrs'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'delinq_2yrs', df_targets_prepr)
+# plot_by_woe(df_temp)
+
+df_inputs_prep['delinq_2yrs:0'] = np.where((df_inputs_prep['delinq_2yrs'] == 0),1,0)
+df_inputs_prep['delinq_2yrs:1-3'] = np.where((df_inputs_prep['delinq_2yrs'] >= 1) & (df_inputs_prep['delinq_2yrs'] <= 3),1,0)
+df_inputs_prep['delinq_2yrs:>=4'] = np.where((df_inputs_prep['delinq_2yrs'] >= 9),1,0)
+
+
+df_inputs_prep['inq_last_6mths'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'inq_last_6mths', df_targets_prepr)
+# plot_by_woe(df_temp,90)
+df_inputs_prep['inq_last_6mths:0'] = np.where((df_inputs_prep['inq_last_6mths'] == 0), 1, 0)
+df_inputs_prep['inq_last_6mths:1-2'] = np.where((df_inputs_prep['inq_last_6mths'] >= 1) & (df_inputs_prep['inq_last_6mths'] <= 2), 1, 0)
+df_inputs_prep['inq_last_6mths:3-6'] = np.where((df_inputs_prep['inq_last_6mths'] >= 3) & (df_inputs_prep['inq_last_6mths'] <= 6), 1, 0)
+df_inputs_prep['inq_last_6mths:>6'] = np.where((df_inputs_prep['inq_last_6mths'] > 6), 1, 0)
+
+
+df_inputs_prep['open_acc'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'open_acc', df_targets_prepr)
+# plot_by_woe(df_temp,90)
+df_inputs_prep['open_acc:0'] = np.where((df_inputs_prep['open_acc'] == 0), 1, 0)
+df_inputs_prep['open_acc:1-3'] = np.where((df_inputs_prep['open_acc'] >= 1) & (df_inputs_prep['open_acc'] <= 3), 1, 0)
+df_inputs_prep['open_acc:4-12'] = np.where((df_inputs_prep['open_acc'] >= 4) & (df_inputs_prep['open_acc'] <= 12), 1, 0)
+df_inputs_prep['open_acc:13-17'] = np.where((df_inputs_prep['open_acc'] >= 13) & (df_inputs_prep['open_acc'] <= 17), 1, 0)
+df_inputs_prep['open_acc:18-22'] = np.where((df_inputs_prep['open_acc'] >= 18) & (df_inputs_prep['open_acc'] <= 22), 1, 0)
+df_inputs_prep['open_acc:23-25'] = np.where((df_inputs_prep['open_acc'] >= 23) & (df_inputs_prep['open_acc'] <= 25), 1, 0)
+df_inputs_prep['open_acc:26-30'] = np.where((df_inputs_prep['open_acc'] >= 26) & (df_inputs_prep['open_acc'] <= 30), 1, 0)
+df_inputs_prep['open_acc:>=31'] = np.where((df_inputs_prep['open_acc'] >= 31), 1, 0)
+
+df_inputs_prep['pub_rec'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'pub_rec', df_targets_prepr)
+# plot_by_woe(df_temp,90)
+df_inputs_prep['pub_rec:0-2'] = np.where((df_inputs_prep['pub_rec'] >= 0) & (df_inputs_prep['pub_rec'] <= 2), 1, 0)
+df_inputs_prep['pub_rec:3-4'] = np.where((df_inputs_prep['pub_rec'] >= 3) & (df_inputs_prep['pub_rec'] <= 4), 1, 0)
+df_inputs_prep['pub_rec:>=5'] = np.where((df_inputs_prep['pub_rec'] >= 5), 1, 0)
+
+df_inputs_prep['total_acc'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'total_acc', df_targets_prepr)
+# plot_by_woe(df_temp,90)
+df_inputs_prep['total_acc:<=27'] = np.where((df_inputs_prep['total_acc'] <= 27), 1, 0)
+df_inputs_prep['total_acc:28-51'] = np.where((df_inputs_prep['total_acc'] >= 28) & (df_inputs_prep['total_acc'] <= 51), 1, 0)
+df_inputs_prep['total_acc:>=52'] = np.where((df_inputs_prep['total_acc'] >= 52), 1, 0)
+
+df_inputs_prep['acc_now_delinq'].unique()
+df_temp = woe_ordered_continuous(df_inputs_prep, 'acc_now_delinq', df_targets_prepr)
+# plot_by_woe(df_temp,90)
+df_inputs_prep['acc_now_delinq:0'] = np.where((df_inputs_prep['acc_now_delinq'] == 0), 1, 0)
+df_inputs_prep['acc_now_delinq:>=1'] = np.where((df_inputs_prep['acc_now_delinq'] >= 1), 1, 0)
+
+# total_rev_hi_lim
+df_inputs_prep['total_rev_hi_lim_factor'] = pd.cut(df_inputs_prep['total_rev_hi_lim'], 2000)
+# Here we do fine-classing: using the 'cut' method, we split the variable into 2000 categories by its values.
+df_temp = woe_ordered_continuous(df_inputs_prep, 'total_rev_hi_lim_factor', df_targets_prepr)
+# We calculate weight of evidence.
+df_temp
+
+# plot_by_woe(df_temp.iloc[: 50, : ], 90)
+df_inputs_prep['total_rev_hi_lim:<=5K'] = np.where((df_inputs_prep['total_rev_hi_lim'] <= 5000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:5K-10K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 5000) & (df_inputs_prep['total_rev_hi_lim'] <= 10000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:10K-20K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 10000) & (df_inputs_prep['total_rev_hi_lim'] <= 20000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:20K-30K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 20000) & (df_inputs_prep['total_rev_hi_lim'] <= 30000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:30K-40K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 30000) & (df_inputs_prep['total_rev_hi_lim'] <= 40000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:40K-55K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 40000) & (df_inputs_prep['total_rev_hi_lim'] <= 55000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:55K-95K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 55000) & (df_inputs_prep['total_rev_hi_lim'] <= 95000), 1, 0)
+df_inputs_prep['total_rev_hi_lim:>95K'] = np.where((df_inputs_prep['total_rev_hi_lim'] > 95000), 1, 0)
+
+df_inputs_prep['annual_inc_factor'] = pd.cut(df_inputs_prep['annual_inc'], 100)
+# Here we do fine-classing: using the 'cut' method, we split the variable into 100 categories by its values.
+df_temp = woe_ordered_continuous(df_inputs_prep, 'annual_inc_factor', df_targets_prepr)
+# We calculate weight of evidence.
+df_temp
+
+# Initial examination shows that there are too few individuals with large income and too many with small income.
+# Hence, we are going to have one category for more than 150K, and we are going to apply our approach to determine
+# the categories of everyone with 140k or less.
+df_inputs_prep_temp = df_inputs_prep.loc[df_inputs_prep['annual_inc'] <= 140000, : ]
+
+df_inputs_prep_temp["annual_inc_factor"] = pd.cut(df_inputs_prep_temp['annual_inc'], 50)
+# Here we do fine-classing: using the 'cut' method, we split the variable into 50 categories by its values.
+df_temp = woe_ordered_continuous(df_inputs_prep_temp, 'annual_inc_factor', df_targets_prepr[df_inputs_prep_temp.index])
+# We calculate weight of evidence.
+df_temp
+# plot_by_woe(df_temp,90)
+# WoE is monotonically decreasing with income, so we split income in 10 equal categories, each with width of 15k.
+df_inputs_prep['annual_inc:<20K'] = np.where((df_inputs_prep['annual_inc'] <= 20000), 1, 0)
+df_inputs_prep['annual_inc:20K-30K'] = np.where((df_inputs_prep['annual_inc'] > 20000) & (df_inputs_prep['annual_inc'] <= 30000), 1, 0)
+df_inputs_prep['annual_inc:30K-40K'] = np.where((df_inputs_prep['annual_inc'] > 30000) & (df_inputs_prep['annual_inc'] <= 40000), 1, 0)
+df_inputs_prep['annual_inc:40K-50K'] = np.where((df_inputs_prep['annual_inc'] > 40000) & (df_inputs_prep['annual_inc'] <= 50000), 1, 0)
+df_inputs_prep['annual_inc:50K-60K'] = np.where((df_inputs_prep['annual_inc'] > 50000) & (df_inputs_prep['annual_inc'] <= 60000), 1, 0)
+df_inputs_prep['annual_inc:60K-70K'] = np.where((df_inputs_prep['annual_inc'] > 60000) & (df_inputs_prep['annual_inc'] <= 70000), 1, 0)
+df_inputs_prep['annual_inc:70K-80K'] = np.where((df_inputs_prep['annual_inc'] > 70000) & (df_inputs_prep['annual_inc'] <= 80000), 1, 0)
+df_inputs_prep['annual_inc:80K-90K'] = np.where((df_inputs_prep['annual_inc'] > 80000) & (df_inputs_prep['annual_inc'] <= 90000), 1, 0)
+df_inputs_prep['annual_inc:90K-100K'] = np.where((df_inputs_prep['annual_inc'] > 90000) & (df_inputs_prep['annual_inc'] <= 100000), 1, 0)
+df_inputs_prep['annual_inc:100K-120K'] = np.where((df_inputs_prep['annual_inc'] > 100000) & (df_inputs_prep['annual_inc'] <= 120000), 1, 0)
+df_inputs_prep['annual_inc:120K-140K'] = np.where((df_inputs_prep['annual_inc'] > 120000) & (df_inputs_prep['annual_inc'] <= 140000), 1, 0)
+df_inputs_prep['annual_inc:>140K'] = np.where((df_inputs_prep['annual_inc'] > 140000), 1, 0)
+
+# mths_since_last_delinq
+# We have to create one category for missing values and do fine and coarse classing for the rest.
+df_inputs_prep_temp = df_inputs_prep[pd.notnull(df_inputs_prep['mths_since_last_delinq'])]
+df_inputs_prep_temp['mths_since_last_delinq_factor'] = pd.cut(df_inputs_prep_temp['mths_since_last_delinq'], 50)
+df_temp = woe_ordered_continuous(df_inputs_prep_temp, 'mths_since_last_delinq_factor', df_targets_prepr[df_inputs_prep_temp.index])
+# We calculate weight of evidence.
+df_temp
+
+# Categories: Missing, 0-3, 4-30, 31-56, >=57
+df_inputs_prep['mths_since_last_delinq:Missing'] = np.where((df_inputs_prep['mths_since_last_delinq'].isnull()), 1, 0)
+df_inputs_prep['mths_since_last_delinq:0-3'] = np.where((df_inputs_prep['mths_since_last_delinq'] >= 0) & (df_inputs_prep['mths_since_last_delinq'] <= 3), 1, 0)
+df_inputs_prep['mths_since_last_delinq:4-30'] = np.where((df_inputs_prep['mths_since_last_delinq'] >= 4) & (df_inputs_prep['mths_since_last_delinq'] <= 30), 1, 0)
+df_inputs_prep['mths_since_last_delinq:31-56'] = np.where((df_inputs_prep['mths_since_last_delinq'] >= 31) & (df_inputs_prep['mths_since_last_delinq'] <= 56), 1, 0)
+df_inputs_prep['mths_since_last_delinq:>=57'] = np.where((df_inputs_prep['mths_since_last_delinq'] >= 57), 1, 0)
+
+#Categories : Debt to income ratio
+# Here we do fine-classing: using the 'cut' method, we split the variable into 100 categories by its values.
+df_inputs_prep['dti_factor'] = pd.cut(df_inputs_prep['dti'], 100)
+# Here we do fine-classing: using the 'cut' method, we split the variable into 50 categories by its values.
+df_inputs_prep['dti_factor'] = pd.cut(df_inputs_prep['dti'], 50)
+# Similarly to income, initial examination shows that most values are lower than 200.
+# Hence, we are going to have one category for more than 35, and we are going to apply our approach to determine
+# the categories of everyone with 150k or less.
+df_inputs_prep_temp = df_inputs_prep.loc[df_inputs_prep['dti'] <= 35, : ]
+# We calculate weight of evidence.
+df_temp = woe_ordered_continuous(df_inputs_prep_temp, 'dti_factor', df_targets_prepr[df_inputs_prep_temp.index])
+# plot_by_woe(df_temp,90)
+# Categories:
+df_inputs_prep['dti:<=1.4'] = np.where((df_inputs_prep['dti'] <= 1.4), 1, 0)
+df_inputs_prep['dti:1.4-3.5'] = np.where((df_inputs_prep['dti'] > 1.4) & (df_inputs_prep['dti'] <= 3.5), 1, 0)
+df_inputs_prep['dti:3.5-7.7'] = np.where((df_inputs_prep['dti'] > 3.5) & (df_inputs_prep['dti'] <= 7.7), 1, 0)
+df_inputs_prep['dti:7.7-10.5'] = np.where((df_inputs_prep['dti'] > 7.7) & (df_inputs_prep['dti'] <= 10.5), 1, 0)
+df_inputs_prep['dti:10.5-16.1'] = np.where((df_inputs_prep['dti'] > 10.5) & (df_inputs_prep['dti'] <= 16.1), 1, 0)
+df_inputs_prep['dti:16.1-20.3'] = np.where((df_inputs_prep['dti'] > 16.1) & (df_inputs_prep['dti'] <= 20.3), 1, 0)
+df_inputs_prep['dti:20.3-21.7'] = np.where((df_inputs_prep['dti'] > 20.3) & (df_inputs_prep['dti'] <= 21.7), 1, 0)
+df_inputs_prep['dti:21.7-22.4'] = np.where((df_inputs_prep['dti'] > 21.7) & (df_inputs_prep['dti'] <= 22.4), 1, 0)
+df_inputs_prep['dti:22.4-35'] = np.where((df_inputs_prep['dti'] > 22.4) & (df_inputs_prep['dti'] <= 35), 1, 0)
+df_inputs_prep['dti:>35'] = np.where((df_inputs_prep['dti'] > 35), 1, 0)
+
+# mths_since_last_record
+# We have to create one category for missing values and do fine and coarse classing for the rest.
+df_inputs_prep_temp = df_inputs_prep[pd.notnull(df_inputs_prep['mths_since_last_record'])]
+#sum(loan_data_temp['mths_since_last_record'].isnull())
+df_inputs_prep_temp['mths_since_last_record_factor'] = pd.cut(df_inputs_prep_temp['mths_since_last_record'], 50)
+# Here we do fine-classing: using the 'cut' method, we split the variable into 50 categories by its values.
+df_temp = woe_ordered_continuous(df_inputs_prep_temp, 'mths_since_last_record_factor', df_targets_prepr[df_inputs_prep_temp.index])
+# We calculate weight of evidence.
+df_temp
+# plot_by_woe(df_temp,90)
+
+# Categories: 'Missing', '0-2', '3-20', '21-31', '32-80', '81-86', '>86'
+df_inputs_prep['mths_since_last_record:Missing'] = np.where((df_inputs_prep['mths_since_last_record'].isnull()), 1, 0)
+df_inputs_prep['mths_since_last_record:0-2'] = np.where((df_inputs_prep['mths_since_last_record'] >= 0) & (df_inputs_prep['mths_since_last_record'] <= 2), 1, 0)
+df_inputs_prep['mths_since_last_record:3-20'] = np.where((df_inputs_prep['mths_since_last_record'] >= 3) & (df_inputs_prep['mths_since_last_record'] <= 20), 1, 0)
+df_inputs_prep['mths_since_last_record:21-31'] = np.where((df_inputs_prep['mths_since_last_record'] >= 21) & (df_inputs_prep['mths_since_last_record'] <= 31), 1, 0)
+df_inputs_prep['mths_since_last_record:32-80'] = np.where((df_inputs_prep['mths_since_last_record'] >= 32) & (df_inputs_prep['mths_since_last_record'] <= 80), 1, 0)
+df_inputs_prep['mths_since_last_record:81-86'] = np.where((df_inputs_prep['mths_since_last_record'] >= 81) & (df_inputs_prep['mths_since_last_record'] <= 86), 1, 0)
+df_inputs_prep['mths_since_last_record:>86'] = np.where((df_inputs_prep['mths_since_last_record'] > 86), 1, 0)
+
+loan_data_inputs_train = df_inputs_prep
+# loan_data_inputs_test = df_inputs_prep
+
+#loan_data_inputs_train.to_csv('loan_data_inputs_train.csv')
+# loan_data_targets_train.to_csv('loan_data_targets_train.csv')
+loan_data_inputs_test.to_csv('loan_data_inputs_test.csv')
+loan_data_targets_test.to_csv('loan_data_targets_test.csv')
