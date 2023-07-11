ae209057749a5aef2fe42ea456321962b24a9e64
BernisM <hellobernis@outlook.com>
2023-07-11 23:13:35 +0200

    don't find coef_ arg

--- PD_model_estimation.py
+++ PD_model_estimation.py
@@ -168,3 +168,48 @@ summary_table.loc[0] = ['Intercept', reg.intercept_[0]]
 summary_table = summary_table.sort_index()
 
 summary_table
+
+## Build a Logistic Regression Model with P-Values
+# P values for sklearn logistic regression.
+
+# Class to display p-values for logistic regression in sklearn.
+
+from sklearn import linear_model
+import scipy.stats as stat
+
+class LogisticRegression_with_p_values:
+    
+    def __init__(self,*args,**kwargs):#,**kwargs):
+        self.model = linear_model.LogisticRegression(*args,**kwargs)#,**args)
+
+    def fit(self,X,y):
+        self.model.fit(X,y)
+        
+        #### Get p-values for the fitted model ####
+        denom = (2.0 * (1.0 + np.cosh(self.model.decision_function(X))))
+        denom = np.tile(denom,(X.shape[1],1)).T
+        F_ij = np.dot((X / denom).T,X) ## Fisher Information Matrix
+        Cramer_Rao = np.linalg.inv(F_ij) ## Inverse Information Matrix
+        sigma_estimates = np.sqrt(np.diagonal(Cramer_Rao))
+        z_scores = self.model.coef_[0] / sigma_estimates # z-score for eaach model coefficient
+        p_values = [stat.norm.sf(abs(x)) * 2 for x in z_scores] ### two tailed test for p-values
+        
+        self.coef_ = self.model.coef_
+        self.intercept_ = self.model.intercept_
+        self.p_values = p_values
+
+# We create an instance of an object from the newly created 'LogisticRegression_with_p_values()' class.
+reg = LogisticRegression_with_p_values()
+
+# Estimates the coefficients of the object from the 'LogisticRegression' class
+# with inputs (independent variables) contained in the first dataframe
+# and targets (dependent variables) contained in the second dataframe.
+reg.fit(inputs_train, loan_data_targets_train)
+
+# Same as above.
+summary_table = pd.DataFrame(columns = ['Feature name'], data = feature_name)
+summary_table['Coefficients'] = np.transpose(reg.coef_)
+summary_table.index = summary_table.index + 1
+summary_table.loc[0] = ['Intercept', reg.intercept_[0]]
+summary_table = summary_table.sort_index()
+summary_table
